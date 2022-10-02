73459ba20c3ed853a3c958738aca2c02eee7e3be
jataubman <jeremy.taubman@generalassemb.ly>
2022-10-02 12:09:03 -0400

    editing readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# U1_data_types_practice
+[![General Assembly Logo](https://camo.githubusercontent.com/1a91b05b8f4d44b5bbfb83abac2b0996d8e26c92/687474703a2f2f692e696d6775722e636f6d2f6b6538555354712e706e67)](https://generalassemb.ly/education/web-development-immersive)
+
+# Data Types Practice
+
+This assignment will give you the opportunity to practice working with
+the fundamentals of JavaScript covered in the prework.
+
+## Prerequisites
+
+* Fundamental JavaScript
+* Numbers, Strings, and Booleans
+* Variables
+* Arrays
+* Array Methods
+
+## Instructions
+
+1.  Fork and clone this repository.
+1.  Fulfill the listed requirements.
+
+## Requirements
+
+There are a series of prompts inside of the starter code found in the Script.js file. Each
+prompt is in the form of a JavaScript comment. Write your code to solve the
+prompt *after* the comment.
+
+You can Use javascript by running `node` on the terminal to run the script as seen below:
+
+```
+
+node script.js
+
+```
+
+> make sure to clone from your own fork! Not from SEIR-1003, as is done in the example above.
+
+Some prompts will require you to practice researching methods we did not learn
+in class.
+
+> **Tip:** Use `console.log` to test your work, but delete those statements when you
+> finish working on the prompt, before moving on to the next one! Otherwise,
+> you'll end up with a lot of clutter in your console and it will be hard to
+> debug the prompt you're currently working on.
+
+**You are required to make one commit after each prompt**
---
+++ script.js
@@ -0,0 +1,156 @@
+/*
+ * Prompt 1:
+ *
+ * Declare a variable called myFavoriteNumber on one line; assign it a value on
+ * the next line.
+ */
+
+/*
+ * Prompt 2:
+ *
+ * Declare 3 variables using each of let, const, and var.
+ */
+
+/*
+ * Prompt 3:
+ *
+ * Create a variable and assign a number to it. On a separate line for each:
+ *   - add a number to your variable
+ *   - subtract a number from your variable
+ *   - multiply your variable by 3
+ *   - divide your variable by 7
+ *   - calculate the remainder of dividing your variable by 2
+ */
+
+/*
+ * Prompt 4:
+ *
+ * Create a variable called firstName and assign it to a string of your first
+ * name. Create a variable called lastName and assign it to a string of your
+ * last name.
+ *
+ * Create a variable called fullName and use addition to combine your firstName
+ * and lastName variables.
+ */
+
+/*
+ * Prompt 5:
+ *
+ * Create a variable called hello and assign it the string 'hello'; create
+ * a variable called world and assign it the string 'world'.
+ *
+ * Create a variable called message and use string interpolation to assign it
+ * the value 'hello world!'.
+ */
+
+/*
+ * Prompt 6:
+ *
+ * Write some code that solves each sub-prompt below.
+ */
+
+// A. Use loose-equals (==) to create a statement that is false
+// B. Use strict equals (===) to create a statement that is true
+// C. Use strict unequal (!==) to create a statement that is true
+// D. Use less than (<) to create a statement that is false
+// E. Use greater than (>) to create a statement that is false
+// F. Use less than or equals (<=) to create a statemetn that is true
+// G. Use greater than or equals (>=) to create a statemetn that is false
+
+/*
+ * Prompt 7:
+ *
+ * Using the variables defined below, replace the underscores in each subprompt so that the statement evaluates to true
+ */
+
+let a = 4;
+let b = 53;
+let c = 57;
+let d = 16;
+let e = 'Kevin';
+
+// a _ b;
+
+// c _ d;
+
+// 'Name' ___ 'Name';
+
+// a _ b ___ c;
+
+// a _ a ___ d;
+
+// e ___ 'Kevin';
+
+// 48 ___ '48';
+
+/*
+ * Prompt 8:
+ *
+ * Using the variables defined below, replace the underscores in each subprompt so that the statement evaluates to false
+ */
+
+a = 6;
+b = 100;
+c = -5;
+d = 3000;
+e = 'Jelly Bean';
+
+// a _ b;
+
+// c _ d;
+
+// 'Peanut' ___ 'Peanut';
+
+// a _ b ___ c;
+
+// a _ a ___ d;
+
+// e ___ 'Jelly Bean';
+
+// 48 ___ '48';
+
+/*
+ * Prompt 9:
+ *
+ * Retrieve "Marty McFly" from each of the arrays below.
+ */
+
+let array1 = ['Marty Marion', 'Marty Feldman', 'Marty McFly', 'Marty Marion'];
+let array2 = [
+  ['Marty Feldman', 'Marty Marion'],
+  ['Marty Stuart', 'Marty McFly'],
+  ['Marty Jannetty', 'Marty Robbins']
+];
+let array3 = [
+  ['Marty Feldman', ['Marty Marion']],
+  ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
+];
+
+/*
+ * Prompt 10:
+ *
+ * Use the length of the array below to retrieve the second to last item.
+ */
+
+let array4 = ['a', 'b', 'c', 'd', 'e'];
+
+/*
+ * Prompt 11:
+ *
+ * Use the following arrays to answer the subprompts below.
+ */
+
+let thom = ['Thom', 1000, 'Christchurch'];
+let karolin = ['Karolin', 16, 'New York'];
+let kristyn = ['Kristyn', 5, 'Pittsburgh'];
+let cathleen = ['Cathleen', 186, 'New York'];
+
+// Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
+// the thom array and replace it with "Gameboy".
+
+// Karolin just had her birthday; change Karolin's array to reflect her being
+// a year older.
+
+// Change Cathleen's hometown from New York to "Gotham City".
+
+// Remove "Pittsburgh" from Kristyn's array and add "Oakland".
